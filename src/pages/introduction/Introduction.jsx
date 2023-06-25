c0bce00a6f0a82971204c01e453eb5fad117c4cc
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-06-25 22:02:23 +0200

    added comign soon floating text on canvas

--- src/pages/introduction/Introduction.jsx
+++ src/pages/introduction/Introduction.jsx
@@ -4,7 +4,14 @@ import './Introduction.css'
 
 export default function Introduction()
 {
-    return <Canvas className='canvas'>
+    return <Canvas className='canvas'
+        camera={ {
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [ 0, 0, 8] 
+        } }    
+    >
         <Experience />
     </Canvas>
 }
--- src/pages/introduction/experience/Experience.jsx
+++ src/pages/introduction/experience/Experience.jsx
@@ -1,12 +1,24 @@
-import { Center, Text, OrbitControls } from '@react-three/drei'
+import { Float, Center, Text, OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
     return <>
         <OrbitControls makeDefault/>
-        <Center />
-        <Text>
-            COMING SOON
-        </Text>
+        <Float 
+            floatIntensity={ 3 } 
+            rotationIntensity={ 3 }
+        >
+            <Text 
+                font="./fonts/bebas-neue-v9-latin-regular.woff"
+                scale={ 1 }
+                maxWidth={ 3.5 }
+                lineHeight={ 0.75 }
+                textAlign='center'
+                position={ [ 0, 0.5, 0 ] }
+            >
+                COMING SOON!
+                <meshBasicMaterial color="black" toneMapped={ false } />
+            </Text>
+        </Float>
     </>
 }
