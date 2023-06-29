24f0ec7f4da8cc2a5d8272456fcb80a37f60a840
Enkhdavaa Batlkhagva <enkhdavaa.batlkhagva@ict.nl>
2023-06-29 16:26:12 +0200

    fixed div

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import Routing from './Routing.jsx'
 
 export default function App()
 {
-    return <>
-        <Header />
-        <Routing />
-        <Footer />
-    </>
+    return <div className='flex flex-col h-screen'>
+        <div className='h-[100px]'>
+            <Header />
+        </div>
+        <div className='flex-grow'>
+            <Routing />
+        </div>
+        <div className='h-[100px]'>
+            <Footer />
+        </div>
+    </div>
 }
