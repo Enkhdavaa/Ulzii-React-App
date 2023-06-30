5e6571365da375228682375e65b40613c2612f82
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-06-30 23:26:01 +0200

    used tailwind for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,8 @@
 import ulziiLogo from './images/Ulzii-Logo.png'
 
-import './Footer.css'
-
 export default function Footer()
 {
-    return <div className='footer'> 
-        <div className='logo'>
-            <img src={ ulziiLogo } />
-        </div>
+    return <div> 
+        <img className='object-cover h-24 w-130' src={ ulziiLogo } />
     </div>
 }
--- src/pages/introduction/Introduction.jsx
+++ src/pages/introduction/Introduction.jsx
@@ -1,6 +1,5 @@
 import { Canvas } from '@react-three/fiber'
 import Experience from './experience/Experience'
-import './Introduction.css'
 
 export default function Introduction()
 {
