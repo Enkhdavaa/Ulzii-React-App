5e2834bbe42fe159ed6a2cc9f9397106c642722c
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-07-04 21:52:23 +0200

    added example images and replaced orbit control with PresentationControl

--- src/pages/introduction/experience/Experience.jsx
+++ src/pages/introduction/experience/Experience.jsx
@@ -1,26 +1,45 @@
-import { Sparkles, Float, Text, OrbitControls } from '@react-three/drei'
+import { Sparkles, Float, Text, PresentationControls } from '@react-three/drei'
 import * as THREE from 'three'
 
 export default function Experience()
 {
     return <>
-        <OrbitControls makeDefault/>
-        <Float 
-            floatIntensity={ 3 } 
-            rotationIntensity={ 3 }
+
+        <PresentationControls
+            rotation={ [ 0.13, 0.1, 0] }
+            polar={ [ - 0.4, 0.2 ] }
+            azimuth={ [ -1, 0.75 ] }
+            config={ { mass: 2, tension: 400 } }
+            snap={ { mass: 4, tension: 400 } }
         >
-            <Text 
-                font="./fonts/bebas-neue-v9-latin-regular.woff"
-                scale={ 1 }
-                maxWidth={ 3.5 }
-                lineHeight={ 0.75 }
-                textAlign='center'
-                position={ [ 0, 0.5, 0 ] }
-                color="green"
+            <Float 
+                floatIntensity={ 3 } 
+                rotationIntensity={ 3 }
             >
-                COMING SOON!
-            </Text>
-        </Float>
+                <Text 
+                    font="./fonts/bebas-neue-v9-latin-regular.woff"
+                    scale={ 1 }
+                    maxWidth={ 3.5 }
+                    lineHeight={ 0.75 }
+                    textAlign='center'
+                    position={ [ 0, 0.5, 0 ] }
+                    color="green"
+                >
+                    COMING SOON!
+                </Text>
+                <Text 
+                    font="./fonts/bebas-neue-v9-latin-regular.woff"
+                    scale={ 0.3 }
+                    maxWidth={ 7 }
+                    lineHeight={ 0.75 }
+                    textAlign='center'
+                    position={ [ 0, -0.5, 0 ] }
+                    color="red"
+                >
+                    (Flip me if you can!)
+                </Text>
+            </Float>
+        </PresentationControls>
         <Sparkles 
             size={ 6 }
             scale={ [ 25, 10, 15 ] }
