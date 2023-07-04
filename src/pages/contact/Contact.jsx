52e0ea2995d725b971b78b67bf2e9b7f92b6eb88
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-07-04 22:17:50 +0200

    added person class

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,11 @@
+import Person from '../../utilities/Person/Person'
+
 export default function Contact()
 {
-    return <>Contact</>
+    return <div className='flex px-5 pt-5 space-x-5 justify-center'>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+    </div>
 }
