70fa76f5e7b539aa6dd26b35b14656823d6d9633
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-07-16 21:46:59 +0200

    added wrapping and scrolling

--- src/utilities/Person/Person.jsx
+++ src/utilities/Person/Person.jsx
@@ -2,7 +2,7 @@ import man from './images/man.png'
 
 export default function Person()
 {
-    return <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+    return <div className='px-5 max-w-sm rounded overflow-hidden shadow-lg'>
         <img className='w-full' src={ man } alt='Person' />
         <div className='px-6 py-4'>
             <div className='font-bold text-xl mb-2'> Here is my name </div>
