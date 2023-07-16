5c0ab8e13270066b9dae58a3fab60c60cfa7a1d4
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-07-16 20:35:05 +0200

    added wrapping and scrolling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,7 +2,7 @@ import Person from '../../utilities/Person/Person'
 
 export default function Contact()
 {
-    return <div className='flex px-5 pt-5 space-x-5 justify-center'>
+    return <div className='flex flex-wrap justify-start px-5 pt-5'>
         <Person />
         <Person />
         <Person />
--- src/utilities/Person/Person.jsx
+++ src/utilities/Person/Person.jsx
@@ -3,7 +3,7 @@ import man from './images/man.png'
 export default function Person()
 {
     return <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-        <img className='w-full' src={ man } alt='Person1' />
+        <img className='w-full' src={ man } alt='Person' />
         <div className='px-6 py-4'>
             <div className='font-bold text-xl mb-2'> Here is my name </div>
             <p className='text-gray-700 text-base'>
