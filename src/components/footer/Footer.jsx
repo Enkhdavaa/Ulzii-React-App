45153c03bb18c00a9a401bfa365b22935212373c
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2023-07-17 21:57:22 +0200

    added margin for footer and contact persons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,6 @@ import ulziiLogo from './images/Ulzii-Logo.png'
 export default function Footer()
 {
     return <div>
-        <img className='object-cover h-14 sm:h-15 md:h-20 lg:h-24' src={ ulziiLogo } />
+        <img className='object-cover mt-5 h-14 sm:h-15 md:h-20 lg:h-24' src={ ulziiLogo } />
     </div>
 }
